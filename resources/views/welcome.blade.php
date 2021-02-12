3ae183015decb80641ee8e0307d03ebb2d188140
farissatyaw <redcolony14@gmail.com>
2021-02-13 02:50:28 +0700

    First Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/belajar.css') }}" rel="stylesheet">
+
+        <title>Faris Satya Wibisana</title>
+
+        <!-- Fonts -->
+        
+
+        <!-- Styles -->
+        <style>
+          
+        </style>
+
+    
+    </head>
+    <body>
+      <header class="py-6 px-8">
+        <div class="container-b">
+          <div class="flex justify-between pb-12">
+              <h1 class="text-4xl font-bold">Xcasts</h1>
+
+              <a class="font-color-white font-bold" href="#">Sign in</a>
+          </div>
+
+          <nav class="flex justify-center">
+          <a href="#">Catalog</a>
+          <a href="#">Series</a>
+          <a href="#">Podcast</a>
+          <a href="#">Discussion</a>
+          </nav>
+        </div>
+      </header>
+    </body>
+</html>
